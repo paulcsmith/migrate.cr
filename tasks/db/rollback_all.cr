c730688c404f956624c05a8201ae7cd49f9fde15
Paul Smith <paulcsmith0218@gmail.com>
2016-12-16 17:31:03 -0500

    Extract common task functionality

--- tasks/db/rollback_all.cr
+++ tasks/db/rollback_all.cr
@@ -1,12 +1,6 @@
 require "./init"
 
-puts "Rolling back all migrations...".colorize(:cyan)
-
-Migrate::Runner.db_name = ARGV.first? || raise "Requires db name passed as first arg"
-
-begin
+Migrate::Task.run do
+  puts "Rolling back all migrations...".colorize(:cyan)
   Migrate::Runner.new.rollback_all
-  puts "✅  Done".colorize(:green)
-rescue e : PQ::PQError
-  puts e.message.colorize(:red)
 end
